5df0512d789b1a019c3804e502a1ce10a980be93
Timur Lagawy <timurtagirovich@mail.ru>
2023-10-10 12:24:49 +0200

    feat: add isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -212,7 +212,16 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  return n % 2 !== 0;
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
